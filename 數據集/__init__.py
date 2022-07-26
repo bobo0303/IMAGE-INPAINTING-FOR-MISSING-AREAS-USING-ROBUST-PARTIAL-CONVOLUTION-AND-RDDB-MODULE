b2daad9cca0dcdd9daefcee55a24517dcad3ee15
BO-WEI, CHEN <56902242+bobo0303@users.noreply.github.com>
2022-07-26 20:26:21 +0800

    Delete 數據集 directory

--- 數據集/__init__.py
+++
@@ -1,5 +0,0 @@
-#from .horse2zebra import horse2zebra_train
-#from .horse2zebra import horse2zebra_test
-#from .lsun_bedroom_line2color import lsun_bedroom_line2color_train
-#from .silverhair import silverhair_train
-from .place2 import place2_train,place2_test,place2_test1
